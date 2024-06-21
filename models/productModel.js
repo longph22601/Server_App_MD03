5b1878392edc60516ed74ca65035d80830af7bd4
NguyenTienPhuong03 <tienphuongnguyen7@gmail.com>
2024-06-22 00:06:24 +0700

    BonusProductSize

--- models/productModel.js
+++ models/productModel.js
@@ -22,6 +22,10 @@ var productSchema = new mongoose.Schema(
       type: Number,
       required: true,
     },
+    size: {
+      type: Number,
+      required: true,
+    },
     category: {
       type: String,
       required: true,
